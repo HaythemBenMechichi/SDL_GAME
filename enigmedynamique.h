2ebaa4aa627b08fad3b156ad85fadcf187b43348
haythem08 <61459758+haythem08@users.noreply.github.com>
2020-03-03 13:51:27 +0100

    Add files via upload

---
+++ enigmedynamique.h
@@ -0,0 +1,26 @@
+#ifndef FONCTIONS_H_
+#define FONCTIONS_H_
+
+typedef struct
+{
+SDL_Surface *temps;
+SDL_Rect postemps;
+}temps;
+typedef struct
+{
+SDL_Surface *icone;
+SDL_Rect posicone;
+}iconeenigme;
+typedef struct
+{
+SDL_Surface *background;
+SDL_Rect posbackground;
+temps time;
+iconeenigme Mat[2][2];
+}enigme;
+void initialiserEnigme(enigme dynamique);
+void afficheEnigme(enigme dynamique);
+void resoudreEnigme(enigme dynamique);
+void gestionDuTempsEnigme(enigme dynamique);
+void generationAleatoirEnigme(enigme dynamique);
+#endif
---
+++ enigmesatatique.h
@@ -0,0 +1,39 @@
+#ifndef FONCTIONS_H_
+#define FONCTIONS_H_
+
+typedef struct
+{
+SDL_Surface *temps;
+SDL_Rect postemps;
+}temps;
+typedef struct
+{
+SDL_Surface *bouton;
+SDL_Rect posbouton;
+}bouton;
+typedef struct
+{
+SDL_Surface *perso;
+SDL_Rect posperso;
+}perso;
+typedef struct
+{
+SDL_Surface *lab;
+SDL_Rect poslab;
+perso charactere;
+}labyrinthe;
+typedef struct
+{
+SDL_Surface *background;
+SDL_Rect posbackground;
+temps time;
+bouton buttom[2];
+}enigme;
+void deplacementperso(labyrinthe laby);
+void afficherlaby(labyrinthe laby);
+void initialiserEnigme(enigme statique);
+void afficheEnigme(enigme statique);
+void resoudreEnigme(enigme statique);
+void gestionDuTempsEnigme(enigme statique);
+void generationAleatoirEnigme(enigme dynamique);
+#endif
